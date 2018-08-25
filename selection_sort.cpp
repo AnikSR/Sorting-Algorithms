3146fcbf7958e2bc5fca90f641ffac217ea4e54b
Anik Samiur Rahman <anik.sr2008@gmail.com>
2018-08-25 17:30:26 +0600

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,5 +1,15 @@
 #include "sort.h"
 
+/* Selection sort is an alter ego of insertion sort. They are basically same algorithm,
+ * seen from different point of views. In the selection sort point of view:
+ * 1. The array is scanned from the beginning to the end for the smallest value.
+ * 2. Once found, it is swapped with the first(i=0) element of the array. Thus the first
+ * element becomes sorted.
+ * 3. The scanning restarts from the next element as the outter for loop advances to i = 1.
+ * Once the smallest of the remaining elements is found, its swapped with with second element
+ * of the array.
+ * 4. This process continues till the last element, sorting the array from smallest element to
+ * the largest. */
 void sSort_s2g(double *Arr, int n){
 	int i, j, k;
        	double tmp;
@@ -17,7 +27,9 @@ void sSort_s2g(double *Arr, int n){
 	}
 }
 
-
+/* To sort the elements in the other direction, the algorithm just have to scan for the highest
+ * element instead of the smallest element in each cycle of scanningand pluck it to the beginning
+ * of the array till the last element. */
 void sSort_g2s(double *Arr, int n){
 	int i, j, k;
 	double tmp;
